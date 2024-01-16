fc969c3928f6e6e5dcbf263eadb3e36e979c12e1
Mazen Al-Madhage <mazen.fp@gmail.com>
2024-01-16 09:35:38 +0300

    added swiper slider to plans card

--- components/PlansSlider/index.tsx
+++ components/PlansSlider/index.tsx
@@ -3,7 +3,24 @@
 import { useContext } from "react";
 import { PlansContext } from "@/contexts/PlansContext";
 import PlanCard from "@/components/PlanCard";
+import { Swiper, SwiperSlide, SwiperProps } from 'swiper/react';
+import { Mousewheel, Pagination } from 'swiper/modules';
+import 'swiper/css/pagination';
+import 'swiper/css';
 import "./index.scss";
+
+const swiperParams: SwiperProps = {
+    slidesPerView: "auto",
+    slideToClickedSlide: true,
+    spaceBetween: 16,
+    modules: [Mousewheel, Pagination],
+    mousewheel: true,
+    pagination: { 
+        clickable: true,
+        el: ".PlansSlider__Pagination",
+    },
+};
+
 
 const PlansSlider: React.FC = () => {
     const { activePlans, currentPlan } = useContext(PlansContext);
@@ -11,12 +28,25 @@ const PlansSlider: React.FC = () => {
     const renderPlans = () => {
         if(!activePlans) return;
 
-        return activePlans.map(el => <PlanCard key={el.id} data={el} isCurrent={currentPlan?.id === el.id}/>)
+        return activePlans.map(el => {
+            return(
+                <SwiperSlide key={el.id} className="PlansSlider__Slide">
+                    <PlanCard data={el} isCurrent={currentPlan?.id === el.id}/>
+                </SwiperSlide>
+            )
+        })
     }
 
     return (
         <div className="PlansSlider">
-            {renderPlans()}
+            <Swiper
+                className="PlansSlider__Swiper"
+                wrapperClass="PlansSlider__SwiperWrapper"
+                {...swiperParams}
+            >
+                {renderPlans()}
+            </Swiper>
+            <div className="PlansSlider__Pagination"></div>
         </div>
     );
 };
